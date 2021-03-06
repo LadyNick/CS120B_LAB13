348e0824ebfb5d1449f9f7919297c51b5a85e027
LadyNick <nnikki1013@gmail.com>
2021-03-06 06:26:52 -0800

    1 progress

--- source/main.c
+++ source/main.c
@@ -10,14 +10,46 @@
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
+#include "timer.h"
+#include "scheduler.h"
 #endif
 
+unsigned short input;
+
+
+//im assuming we can just change the data type to short, and the i <8 to i < 10
+void transmit_data(unsigned short data) {
+    int i;
+        for (i = 0; i < 10 ; ++i) {
+        // Sets SRCLR to 1 allowing data to be set
+        // Also clears SRCLK in preparation of sending data
+        PORTD = 0x08;
+            // set SER = next bit of data to be sent.
+            PORTD |= ((data >> i) & 0x01);
+            // set SRCLK = 1. Rising edge shifts next bit of data into the shift register
+            PORTD |= 0x02;  
+        }
+        // set RCLK = 1. 
+        PORTD |= 0x04;
+    }
+}
+
+void A2D_init() {
+      ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+	// ADEN: Enables analog-to-digital conversion
+	// ADSC: Starts analog-to-digital conversion
+	// ADATE: Enables auto-triggering, allowing for constant
+	//	    analog to digital conversions.
+}
+
+
 int main(void) {
-    /* Insert DDR and PORT initializations */
+    
 
-    /* Insert your solution below */
+    A2D_init();
+
     while (1) {
-
+	input = ADC;
     }
     return 1;
 }
